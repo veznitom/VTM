457a2e873e1927405b50f57f3610c1c209492f36
veznitom <tomas.veznik@seznam.cz>
2023-06-04 00:05:32 +0200

    Naming scheme change, beginning of fixes, part of custom circular queue added.

---
+++ src/Structures.sv
@@ -0,0 +1,114 @@
+package Structures;
+
+  typedef enum reg [7:0] {
+    LB,
+    LH,
+    LW,
+    LBU,
+    LHU,
+    SB,
+    SH,
+    SW,
+    BEQ,
+    BNE,
+    BLT,
+    BGE,
+    BLTU,
+    BGEU,
+    JAL,
+    JALR,
+    ADDI,
+    SLTI,
+    SLTIU,
+    XORI,
+    ORI,
+    ANDI,
+    SLLI,
+    SRLI,
+    SRAI,
+    ADD,
+    SUB,
+    SLL,
+    SLT,
+    SLTU,
+    XOR,
+    SRL,
+    SRA,
+    OR,
+    AND,
+    LUI,
+    AUIPC,
+    ECALL,
+    EBREAK,
+    FENCE,
+    UNKNOWN
+  } instr_name_e;
+ 
+  typedef enum reg [2:0] {
+    LOAD,
+    PREPARE,
+    RENAME,
+    FETCH,
+    ISSUE,
+    STALL,
+    BREAK
+  } dispatch_state_e;
+
+  typedef enum reg [2:0] {
+    NJ1NJ2,
+    NJ1J2,
+    J1NJ2,
+    J1J2,
+    ERROR
+  } instr_types_e;
+
+  typedef enum reg [1:0] {
+    BYTE,
+    HALFWORD,
+    WORD,
+    EMPTY
+  } data_width_e;
+
+  typedef enum reg [1:0] {
+    VALID,
+    WAITING,
+    INVALID,
+    MODIFIED
+  } cache_state_e;
+
+  typedef enum reg [1:0] {
+    IDLE,
+    READ,
+    WRITE
+  } mmu_state_e;
+
+  typedef struct packed {
+    reg [31:0] data1, data2, address, imm;
+    instr_name_e instr_name;
+    reg [5:0] src1, src2, rrn;
+    reg valid1, valid2, tag, ignore;
+  } station_record_t;
+
+  typedef struct packed {
+    reg [31:0] value;
+    reg [5:0] rrn;
+    reg valid, tag;
+  } register_t;
+
+  typedef struct packed {
+    reg [31:0] data, address, instr;
+    cache_state_e state;
+    word_select_e ws;
+  } data_cache_record_t;
+
+  typedef struct packed {reg [31:0] address, instr;} instr_cache_record_t;
+
+  typedef struct packed {
+    reg [31:0] data, address, jump_address;
+    reg [5:0] arn, rrn;
+    reg finished, jump, tag, ignore;
+  } rob_record_t;
+endpackage
+
+
+
--- src/interfaces/control.sv
+++ src/interfaces/control.sv
@@ -1,36 +1,27 @@
-interface GlobalSignals(input logic clk, reset);
+interface global_signals_if (
+    input logic clk,
+    reset
+);
   logic delete_tagged, clear_tags;
 
-  modport rob(
-    input clk, reset,
-    output delete_tagged, clear_tags
-  );
+  modport rob(input clk, reset, output delete_tagged, clear_tags);
 
-  modport rest (
-    input clk, reset, delete_tagged, clear_tags
-  );
+  modport rest(input clk, reset, delete_tagged, clear_tags);
 endinterface
 
-interface DebugInterface;
+interface debug_interface_if;
   logic [31:0] reg11_value;
-  logic [6:0] ren_queue_size;
+  logic [ 6:0] ren_queue_size;
 endinterface
 
-interface PCInterface;
+interface pc_interface_if;
   logic [31:0] jump_address, address;
   logic inc, inc2, wr;
 
-  modport pc (
-    input jump_address, inc, inc2, wr,
-    output address
-  );
+  modport pc(input jump_address, inc, inc2, wr, output address);
 
-  modport dispatch(
-    input address,
-    output inc, inc2
-  );
+  modport dispatch(input address, output inc, inc2);
 
-  modport rob (
-    output jump_address, wr
-  );
+  modport rob(output jump_address, wr);
 endinterface
+
